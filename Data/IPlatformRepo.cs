046ec9e1445eab4381693088674006857980ba3c
dc morrison <nosirromd@gmail.com>
2022-05-13 22:44:46 +0100

    define repo interface

---
+++ Data/IPlatformRepo.cs
@@ -0,0 +1,13 @@
+using RedisAPI2.Models;
+
+namespace RedisAPI2.Data
+{
+    public interface IPlatformRepo
+    {
+        void CreatePlatform(Platform plat);
+        Platform GetPlatformById(string id);
+        IEnumerable<Platform> GetAllPlatforms();
+    }
+
+
+}
