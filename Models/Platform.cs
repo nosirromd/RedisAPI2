3235f6a1b9de03b6312f00222e883d3bd38e2239
dc morrison <nosirromd@gmail.com>
2022-05-13 22:11:43 +0100

    both class fields are required

--- Models/Platform.cs
+++ Models/Platform.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace RedisAPI2.Models
 {
     public class Platform
     {
+        [Required]
         public string Id { get; set; } = $"platform:{Guid.NewGuid().ToString()}";
 
+        [Required]
         public string Name { get; set; } = string.Empty;
     }
 }
