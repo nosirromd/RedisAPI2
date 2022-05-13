c1c63a8aecb8a730ec018746ec53ef82ddba9b24
dc morrison <nosirromd@gmail.com>
2022-05-13 21:59:49 +0100

    coerce guid to string

--- Models/Platform.cs
+++ Models/Platform.cs
@@ -2,7 +2,7 @@ namespace RedisAPI2.Models
 {
     public class Platform
     {
-        public string Id { get; set; } = $"platform:{Guid.NewGuid()}";
+        public string Id { get; set; } = $"platform:{Guid.NewGuid().ToString()}";
 
         public string Name { get; set; } = string.Empty;
     }
