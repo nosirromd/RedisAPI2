272e2d4faa85f2f195758fca1c51d25be2c4b092
dc morrison <nosirromd@gmail.com>
2022-05-15 00:41:16 +0100

    add post and get platform by id routes

--- Controllers/PlatformsController.cs
+++ Controllers/PlatformsController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using RedisAPI2.Data;
+using RedisAPI2.Models;
 
 namespace RedisAPI2.Controllers
 {
@@ -11,6 +12,25 @@ namespace RedisAPI2.Controllers
         public PlatformsController(IPlatformRepo repo)
         {
             _repo = repo;
+        }
+
+        [HttpGet("{id}", Name="GetPlatformById")]
+        public ActionResult<Platform> GetPlatformById(string id)
+        {
+            var platform = _repo.GetPlatformById(id);
+            if (platform != null)
+            {
+                return Ok(platform);
+            } 
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<Platform> CreatePlatform(Platform platform)
+        {
+            _repo.CreatePlatform(platform);
+
+            return CreatedAtRoute(nameof(GetPlatformById), new {Id=platform.Id}, platform);
         }
     }
 }
